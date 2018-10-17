8c9b090525f4a7a94b3614d9f7820e275220a360
MichalHalucha <michal.halucha@gmail.com>
2018-10-17 14:17:37 +0200

    Gra słów

---
+++ Rozdzial9.py
@@ -0,0 +1,31 @@
+
+
+#Wzorzec AVOID
+#def avoid(word,forbridden):
+#    for letter in word:
+#        if letter in forbridden:
+#            return False
+#    return True
+
+
+#WZORZEC WYSZUKIWANIA
+#def searching(word):
+#    for letter in word:
+#        if letter =='e':
+#            return False
+#        return True
+
+
+
+#Odczytywanie pliku
+#fin = open("words.txt")
+#for lines in fin:
+#    word = lines.strip()
+#    if len(word)>20:
+#        print(word)
+
+
+#Czytanie lini
+#fin = open('words.txt','r')
+#line = fin.readline()
+#print(line)
