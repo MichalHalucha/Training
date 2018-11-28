8f58da15996977466317f97b9df78199ae0f5250
MichalHalucha <44180583+MichalHalucha@users.noreply.github.com>
2018-11-28 11:47:16 +0100

    json

--- json.py
+++ json.py
@@ -1,4 +1,21 @@
 import json
+
+def greet_user():
+    """Greet the user by name."""
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        username = input("What is your name? ")
+        with open(filename, 'w') as f_obj:
+            json.dump(username, f_obj)
+        print("We'll remember you when you come back, " + username + "!")
+    else:
+        print("Welcome back, " + username + "!")
+
+
+
 numbers = [2, 3, 5, 7, 11, 13]
 filename = 'numbers.json'
 with open(filename, 'w') as f_obj:
@@ -10,3 +27,5 @@ with open(filename) as f_obj:
     #json.dump(username, f_obj)
     numbers = json.load(f_obj)
     print(numbers)
+
+
