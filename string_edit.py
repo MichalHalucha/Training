e9d5fbc2d97a4662a5c66e63600229244140c09a
MichalHalucha <44180583+MichalHalucha@users.noreply.github.com>
2020-06-12 13:32:49 +0200

    explanation

---
+++ string_edit.py
@@ -0,0 +1,19 @@
+capitalize() - pierwsza litera wielka
+isalnum() - zwraca true gdy ciag zawiera przynajmniej jeden znak
+isalpha() - zwraca true gdy jest przynajmniej jeden znak
+isdecimal - true gdy ciag zawiera znaki liczb dziesietnych 
+isdigit() - true gdy tylko cyfry
+islower(), isupper()
+isnumeric() - true gdy posiada tylko znaki tworzace liczby 
+isspace() - true gdy ciag zawiera tylko spacje 
+istitle () - true gdy Pan Tadeusz
+join(seq) - "Witaj", seq = "!*!" wynik = !Witaj*Witaj!
+len() - dlugosc
+lower()
+lstrip(),rstrip() - usuwa pocz/kon spacje
+max(), min()
+split(str="ogranicznik",num=string.count(str)) - rozdzielacz
+splitlines(num=string.count("\n"))
+strip() - usuwa spacje z pocz i konca 
+upper()
+zfill(width) - dopełnienie zerami
