3a80929cd8885c09904eed76e7d2b95dc5f4a075
MichalHalucha <44180583+MichalHalucha@users.noreply.github.com>
2020-06-12 14:06:22 +0200

    Done

---
+++ string_search.py
@@ -0,0 +1,6 @@
+count(str,beg=0,end=len(string)) - ile razy występuje parametr string
+endswith(suffix,beg=0,end=len(string)) - true jeżeli ciąg kończy się na suffix
+find/index(str,beg=0,end=len(string)) = true jeżeli str w ciągu i wyświetla index
+rfind, rindex - od końca
+startswitch(prefix,beg=0, end=len(string)) - True gdy ciąg rozpoczyna się od znaków specialnych określonych przez prefix
+replace("zmiana z", "zmiana na")
