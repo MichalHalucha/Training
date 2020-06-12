14b835a664aad4baacc16926837a16fcf94f82c2
MichalHalucha <44180583+MichalHalucha@users.noreply.github.com>
2020-06-12 14:39:59 +0200

    initial

---
+++ list_method.py
@@ -0,0 +1,11 @@
+append - dodaje nowy
+clear - usuwa wszystko
+copy - kopiuje
+count - 
+extend - pobiera z istniejącej i dodaje do bieżącej 
+index
+insert - dodaje w konkretne miejsce
+pop - usuwa z konca
+remove - usuwa z konkretnego miejsca
+reverse
+sort
